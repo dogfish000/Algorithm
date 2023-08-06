9cb89c0c622cb48916324e0c70715b5f55a6ab7b
dogfish000 <125009130+dogfish000@users.noreply.github.com>
2023-08-06 18:30:06 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.py
@@ -0,0 +1,16 @@
+N = int(input())
+
+number = 1
+for i in range(1, N + 1):
+    number *= i
+
+number = str(number)
+
+cnt = 0
+for i in range(len(number) - 1, -1, -1):
+    if number[i] != '0':
+        print(cnt)
+        break
+
+    if number[i] == '0':
+        cnt += 1
