ea0d3d1129e3562bc2caa639ad2dabd29afd78b8
dogfish000 <125009130+dogfish000@users.noreply.github.com>
2023-08-06 17:35:17 +0900

    [Bronze II] Title: Hashing, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -1,11 +1,13 @@
 L = int(input())
 
 answer = 0
+hash_val = 1234567891
 
 lst = list(input())
 
 for i in range(L):
     num = ord(lst[i]) - ord('a') + 1
     answer += num * (31 ** i)
+    answer %= hash_val
 
 print(answer)
