0d2402da2262c7298048ca43bfceace456a0a322
dogfish000 <125009130+dogfish000@users.noreply.github.com>
2023-08-11 16:19:36 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -0,0 +1,16 @@
+T = int(input())
+
+dp = [0] * 12
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
+
+for i in range(4, 12):
+    dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+
+
+for tc in range(1, T + 1):
+    n = int(input())
+
+    print(dp[n])
+
