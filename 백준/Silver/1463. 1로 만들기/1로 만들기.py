aafbf5f0dd566099a105984f1cdfaa1afb82e617
dogfish000 <125009130+dogfish000@users.noreply.github.com>
2023-08-11 10:46:21 +0900

    [Silver III] Title: 1로 만들기, Time: 564 ms, Memory: 39068 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -0,0 +1,21 @@
+N = int(input())
+
+dp = [N] * (N + 1)
+
+
+dp[1] = 0
+
+for i in range(1, N):
+    if i * 3 <= N:
+        if dp[i * 3] > dp[i] + 1:
+            dp[i * 3] = dp[i] + 1
+
+    if i * 2 <= N:
+        if dp[i * 2] > dp[i] + 1:
+            dp[i * 2] = dp[i] + 1
+
+    if i + 1 <= N:
+        if dp[i + 1] > dp[i] + 1:
+            dp[i + 1] = dp[i] + 1
+
+print(dp[N])
