e868310d763ee3f8add9d02ede03d6009d18e789
dogfish000 <125009130+dogfish000@users.noreply.github.com>
2023-08-18 14:25:22 +0900

    [Silver I] Title: IOIOI, Time: 164 ms, Memory: 116868 KB -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -3,33 +3,21 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-
 M = int(input())
+S = input()
 
-S = input().rstrip()
+count = 0
+Pattern = 0
+i = 1
+while i < M - 1:
+    if S[i - 1] == 'I' and S[i] == 'O' and S[i + 1] == 'I':
+        Pattern += 1
+        if Pattern == N:
+            Pattern -= 1
+            count += 1
+        i += 1
+    else:
+        Pattern = 0
+    i += 1
 
-def oioi(N):
-    res = 'I'
-
-    for i in range(N):
-        res += "OI"
-
-    return res
-
-cnt = 0
-find_word = oioi(N)
-
-while True:
-    cnt += S.count(find_word)
-
-    if not find_word in S:
-        break
-
-
-    S = S.replace(oioi(N), oioi(N - 1))
-
-
-
-
-
-print(cnt)
+print(count)
