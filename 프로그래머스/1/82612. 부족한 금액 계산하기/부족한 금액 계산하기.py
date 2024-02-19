d0458b7b823aba6962bd46ec456400ad14fdaf7b
dogfish000 <125009130+dogfish000@users.noreply.github.com>
2024-02-19 11:15:34 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.06 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.py
@@ -0,0 +1,12 @@
+def solution(price, money, count):
+    answer = -1
+    
+    total_price = 0
+    for i in range(1, count + 1):
+        total_price += i * price
+    
+    answer = total_price - money
+    if answer < 0:
+        answer = 0
+    
+    return answer
