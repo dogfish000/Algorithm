c23891efca0d125473eae819a24fe3b397368acd
dogfish000 <125009130+dogfish000@users.noreply.github.com>
2024-02-26 15:32:20 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -0,0 +1,14 @@
+def solution(cards1, cards2, goal):
+    answer = 'Yes'
+    
+    for g in goal:
+        if cards1 and cards1[0] == g:
+            cards1.remove(g)
+        elif cards2 and cards2[0] == g:
+            cards2.remove(g)
+        else:
+            answer = 'No'
+            break
+    
+    
+    return answer
