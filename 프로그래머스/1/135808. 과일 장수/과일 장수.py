0cc4ac421c1c46f566cebc44c541a1d620cde106
dogfish000 <125009130+dogfish000@users.noreply.github.com>
2024-02-26 20:57:11 +0900

    [level 1] Title: 과일 장수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.py
@@ -0,0 +1,11 @@
+def solution(k, m, score):
+    answer = 0
+    
+    score.sort(reverse=True)
+    
+    for i in range(m - 1, len(score), m):
+        answer += score[i]
+        
+    answer *= m
+    
+    return answer
